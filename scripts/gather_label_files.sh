8ea4fbb16cb8be4c72b304e76bc0e3b79ed418a1
David Brown <david.brown@lifeomic.com>
2022-05-03 22:39:16 -0600

    Add dicom2nifti dependency

--- scripts/gather_label_files.sh
+++ scripts/gather_label_files.sh
@@ -10,4 +10,4 @@ cd $DIR/..
 python -m src.gather_labeled_files \
     --search-dir /scratch/groups/jdf1/ribfrac/irb58665-rit \
     --labels ./data/stanford/labels.csv \
-    --out-dir ./data/stanford/dicoms/
+    --out-dir ./data/stanford/compressed_dicoms/
