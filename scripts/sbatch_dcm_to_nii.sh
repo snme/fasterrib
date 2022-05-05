27ace6c47d91283b90765273c183db60355eec47
David Brown <david.brown@lifeomic.com>
2022-05-05 13:44:52 -0600

    add sbatch dcm to nii script

--- scripts/sbatch_dcm_to_nii.sh
+++ scripts/sbatch_dcm_to_nii.sh
@@ -4,11 +4,12 @@
 #SBATCH --error=train-err.txt
 #SBATCH --time=48:00:00
 #SBATCH  -p owners
-#SBATCH --mem-per-cpu=64B
+#SBATCH --mem-per-cpu=64GB
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=davidwb
+#SBATCH --partition=bigmem
 
 set -eo pipefail
 
@@ -25,3 +26,4 @@ python -m src.dcm_to_nii \
     --search-dir ./data/stanford/dicoms \
     --labels ./data/stanford/labels.csv \
     --out-dir ./data/stanford/niis/
+    
