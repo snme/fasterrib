d30e660acbde89aa3fb51ad751b8e5611549466d
David Brown <david.brown@lifeomic.com>
2022-05-18 21:33:19 -0600

    add data preparation script

---
+++ src/prepare_ribfrac_data.py
@@ -0,0 +1,104 @@
+import argparse
+import csv
+import os
+from pathlib import Path
+
+import nibabel as nib
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(
+    description="Prepares RibFrac Challenge images and labels for training and evaluation"
+)
+
+dirname = os.path.dirname(__file__)
+train_images_dir = os.path.join(
+    dirname, "../data/ribfrac-challenge/training/images/all"
+)
+train_labels_dir = os.path.join(
+    dirname, "../data/ribfrac-challenge/training/labels/all"
+)
+train_info_path = os.path.join(
+    dirname, "../data/ribfrac-challenge/training/ribfrac-train-info-all.csv"
+)
+val_images_dir = os.path.join(dirname, "data/ribfrac-challenge/validation/images/all")
+val_labels_dir = os.path.join(dirname, "data/ribfrac-challenge/validation/labels/all")
+val_info_path = os.path.join(
+    dirname, "../data/ribfrac-challenge/validation/ribfrac-val-info.csv"
+)
+
+
+train_out_dir = os.path.join(dirname, "../data/ribfrac-challenge/training/prepared/")
+val_out_dir = os.path.join(dirname, "../data/ribfrac-challenge/validation/prepared/")
+
+
+def prepare_data(img_dir, label_dir, info_path, out_dir):
+    label_id_to_code = {}
+    with open(info_path, newline="") as f:
+        reader = csv.DictReader(f, delimiter=",")
+        for row in reader:
+            label_id = int(row["label_id"])
+            code = int(row["label_code"])
+            label_id_to_code[label_id] = code
+
+    label_ids = np.array(list(label_id_to_code.keys()))
+    codes = np.array(list(label_id_to_code.values()))
+    label_mapping = np.zeros(label_ids.max() + 1, dtype=codes.dtype)
+    label_mapping[label_ids] = codes
+
+    img_paths = [path for path in Path(img_dir).glob("*.nii.gz")]
+    img_paths.sort()
+    label_paths = [path for path in Path(label_dir).glob("*.nii.gz")]
+    label_paths.sort()
+
+    assert len(img_paths) == len(label_paths)
+
+    for i, (img_path, label_path) in tqdm(
+        enumerate(zip(img_paths, label_paths)), total=len(img_paths)
+    ):
+        img = nib.load(img_path).get_fdata().astype(np.float32)
+
+        label = nib.load(label_path).get_fdata().astype(np.int8)
+
+        assert label.shape == img.shape
+
+        img = img.transpose(2, 0, 1)  # (slices, W, H)
+        label = label.transpose(2, 0, 1)  # (slices, W, H)
+
+        n_slices = img.shape[0]
+
+        img = torch.as_tensor(label, dtype=torch.float32)
+        label = torch.as_tensor(label, dtype=torch.long)
+
+        # map all label ids to codes
+        label.apply_(label_id_to_code.get)
+        label += 1  # Force labels to be in range [0, 5]. https://zenodo.org/record/3893508#.YoL-wnXMJH5
+
+        assert label.min() >= 0
+        assert label.max() <= 5
+
+        label = torch.nn.functional.one_hot(label, num_classes=6)
+
+        assert list(label.size()) == [n_slices, 512, 512, 6]
+
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        out_path = os.path.join(out_dir, f"img_label_pair_{i}.pt")
+        torch.save((img, label), out_path)
+
+
+def main():
+    print("Preparing training data")
+    prepare_data(
+        img_dir=train_images_dir,
+        label_dir=train_labels_dir,
+        info_path=train_info_path,
+        out_dir=train_out_dir,
+    )
+
+
+if __name__ == "__main__":
+    parser.parse_args()
+    main()
