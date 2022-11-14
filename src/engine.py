2b94323515305ac53ea9af1fc3d408386b2f1b8c
Sathya Edamadaka <sath@stanford.edu>
2022-11-14 05:46:01 +0000

    feat: gpu

--- src/engine.py
+++ src/engine.py
@@ -15,7 +15,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
     metric_logger = utils.MetricLogger(delimiter="  ")
     metric_logger.add_meter("lr", utils.SmoothedValue(window_size=1, fmt="{value:.6f}"))
     header = f"Epoch: [{epoch}]"
-    losses = []
+    loss_history = []
 
     lr_scheduler = None
     if epoch == 0:
@@ -39,7 +39,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
 
         loss_value = losses_reduced.item()
-        losses.append(loss_value)
+        loss_history.append(loss_value)
 
         print(f"Loss is {loss_value}")
         if not math.isfinite(loss_value):
@@ -62,7 +62,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
-    return metric_logger, torch.Tensor(losses)
+    return metric_logger, loss_history
 
 
 def _get_iou_types(model):
